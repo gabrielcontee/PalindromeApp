3e42014d118d3fb2a2851a9eb01f7b81f6c25143
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-12 14:07:41 -0300

    Xcode project created

---
+++ AegroApp/AegroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AegroApp
+//
+//  Created by Gabriel Conte on 12/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
