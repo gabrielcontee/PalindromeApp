bdf3089323bbfd77160037e3178805ed23679227
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-12 14:56:48 -0300

    Created basic project folder structure and classes

---
+++ AegroApp/AegroApp/Model/Words.swift
@@ -0,0 +1,9 @@
+//
+//  Words.swift
+//  AegroApp
+//
+//  Created by Gabriel Conte on 12/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import Foundation
---
+++ AegroApp/AegroApp/ViewModel/PalindromeTestViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PalindromeTestViewModel.swift
+//  AegroApp
+//
+//  Created by Gabriel Conte on 12/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import Foundation
+
+class  PalindromeTestViewModel: NSObject {
+    
+    
+}
