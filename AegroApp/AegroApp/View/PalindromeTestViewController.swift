651f896bac4b370efe3221f6414020644994f8f9
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-12 15:10:54 -0300

    Text field word change recognition method implemented

--- AegroApp/AegroApp/View/PalindromeTestViewController.swift
+++ AegroApp/AegroApp/View/PalindromeTestViewController.swift
@@ -13,5 +13,24 @@ class PalindromeTestViewController: UIViewController{
     @IBOutlet weak var palindromeTestTextField: UITextField!
     @IBOutlet weak var palindromeTestResultLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        palindromeTestTextField.delegate = self as UITextFieldDelegate
+        palindromeTestTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
     
+}
+
+extension PalindromeTestViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        palindromeTestTextField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        print(textField.text)
+    }
 }
