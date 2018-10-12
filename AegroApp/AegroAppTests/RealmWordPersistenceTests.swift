99449f543e5a0f067b3b47a968558965e0ee3639
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-12 19:41:35 -0300

    Realm pod installed

---
+++ AegroApp/AegroAppTests/RealmWordPersistenceTests.swift
@@ -0,0 +1,16 @@
+//
+//  RealmWordPersistenceTests.swift
+//  AegroAppTests
+//
+//  Created by Gabriel Conte on 12/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+
+@testable import AegroApp
+
+class RealmWordPersistenceTests: XCTestCase {
+
+}
